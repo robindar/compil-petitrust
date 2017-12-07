c92aaa076841d010e7b3ea019cb772be3e7c5c8f
David ROBIN <david.robin97@gmail.com>
2017-12-07 12:09:29 +0100

    Add support for command line options

--- main.ml
+++ main.ml
@@ -21,7 +21,36 @@ let rec print_expr fmt = function
   | Len e -> fprintf fmt "(%a) .len()" print_expr e
   | Brackets (e, i) -> fprintf fmt "(%a) [%a]" print_expr e print_expr i
 
-let buf = Lexing.from_channel stdin
-let e = Parser.main Lexer.token buf
 
-(* let () = printf "%a@." print e *)
+let usage = "usage: prustc [options] file.rs"
+
+let parse_only = ref false
+
+let spec =
+  [
+    "--parse-only", Arg.Set parse_only, "  stop after parsing";
+  ]
+
+let file =
+  let file = ref None in
+  let set_file s =
+    if not (Filename.check_suffix s ".rs") then
+      raise (Arg.Bad "no .rs extension");
+    file := Some s
+  in
+  Arg.parse spec set_file usage;
+  match !file with Some f -> f | None -> Arg.usage spec usage; exit 1
+
+
+let () =
+  let c = open_in file in
+  let lb = Lexing.from_channel c in
+  try
+    let f = Parser.file Lexer.token lb in
+    close_in c;
+    if !parse_only then exit 0;
+    (* Move on *)
+  with
+    | e ->
+	eprintf "Anomaly: %s\n@." (Printexc.to_string e);
+	exit 2
